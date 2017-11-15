df62eb42a38eb869b79e5cd2c8f7bd54bc0bdb58
Muchhal, Baljeet (DS) <Baljeet.Muchhal@cdk.com>
2017-11-15 16:38:28 +0530

    Error handling code

---
+++ fileUpload/src/main/java/com/test/model/BatchRunData.java
@@ -0,0 +1,41 @@
+package com.test.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BatchRunData {
+    private int successRecords;
+    private int failedRecords;
+
+    public int getSuccessRecords() {
+        return successRecords;
+    }
+
+    public void setSuccessRecords(int successRecords) {
+        this.successRecords = successRecords;
+    }
+
+    public int getFailedRecords() {
+        return failedRecords;
+    }
+
+    public void setFailedRecords(int failedRecords) {
+        this.failedRecords = failedRecords;
+    }
+
+    public void increamentSuccessRecords(){
+        successRecords++;
+    }
+
+    public void increamentFailedRecords(){
+        failedRecords++;
+    }
+
+    @Override
+    public String toString() {
+        return "BatchRunData{" +
+                "successRecords=" + successRecords +
+                ", failedRecords=" + failedRecords +
+                '}';
+    }
+}
--- fileUpload/src/main/java/com/test/repository/FileDataRepository.java
+++ fileUpload/src/main/java/com/test/repository/FileDataRepository.java
@@ -1,23 +1,30 @@
 package com.test.repository;
 
+import com.test.model.BatchRunData;
 import com.test.model.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.BatchUpdateException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class FileDataRepository {
 
-    private static final Logger log = LoggerFactory.getLogger(FileDataRepository.class);
+    private static final Logger logger = LoggerFactory.getLogger(FileDataRepository.class);
 
     @Autowired
     JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    BatchRunData batchRunData;
 
     @Value("${com.test.batch.size}")
     int batchSize;
@@ -26,22 +33,51 @@ public class FileDataRepository {
 
         jdbcTemplate.execute("DROP TABLE employees IF EXISTS");
         jdbcTemplate.execute("CREATE TABLE employees(" +
-                "id INT , first_name VARCHAR(255), last_name VARCHAR(255), birthdate VARCHAR(255), SALARY DOUBLE(10.2))");
+                "id INT PRIMARY KEY, first_name VARCHAR(255) UNIQUE , last_name VARCHAR(255), birthdate VARCHAR(255), SALARY DOUBLE(10.2))");
 
         List<Object[]> splitRecord = result.stream().skip(1)
                 .map(record -> record.split(","))
                 .collect(Collectors.toList());
 
-        for (int j = 0; j < splitRecord.size(); j += batchSize) {
-            final List<Object[]> batchList = splitRecord.subList(j, j + batchSize > splitRecord.size() ? splitRecord.size() : j + batchSize);
-            jdbcTemplate.batchUpdate("INSERT INTO employees(id,first_name, last_name,birthdate,salary) VALUES (?,?,?,?,?)", batchList);
+
+            for (int j = 0; j < splitRecord.size(); j += batchSize) {
+                final List<Object[]> batchList = splitRecord.subList(j, j + batchSize > splitRecord.size() ? splitRecord.size() : j + batchSize);
+                try {
+                      jdbcTemplate.batchUpdate("INSERT INTO employees(id,first_name, last_name,birthdate,salary) VALUES (?,?,?,?,?)", batchList);
+                      batchRunData.setSuccessRecords(batchRunData.getSuccessRecords()+batchSize);
+
+
+            }catch (Exception e) {
+                    if (e.getCause() instanceof BatchUpdateException) {
+                        BatchUpdateException be = (BatchUpdateException) e.getCause();
+                        int[] batchRes = be.getUpdateCounts();
+                        if (batchRes != null && batchRes.length > 0) {
+                            for (int index = 0; index < batchRes.length; index++) {
+                                if (batchRes[index] == Statement.EXECUTE_FAILED) {
+                                    logger.error("Error execution >>>>>>>>>>>"
+                                            + index + " --- , codeFail : " + batchRes[index]
+                                            + "---, line " + splitRecord.get(index));
+                                }
+                            }
+                        }
+                    }
+                    if (e instanceof DuplicateKeyException) {
+                        DuplicateKeyException be = (DuplicateKeyException) e;
+                        logger.error("Record already exist ");
+                    }
+
+                    batchRunData.increamentFailedRecords();
+
+                    continue;
+                }
 
         }
 
         jdbcTemplate.query(
-                "SELECT id, first_name, last_name,birthdate,salary FROM employees WHERE id = ?", new Object[] { 1},
+                "SELECT id, first_name, last_name,birthdate,salary FROM employees WHERE id = ?", new Object[] { 4},
                 (rs, rowNum) -> new Employee(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"),rs.getString("birthdate"),rs.getDouble("salary"))
-        ).forEach(employee -> log.info("Read employee "+employee.toString()));
+        ).forEach(employee -> logger.info("Read employee "+employee.toString()));
 
+        logger.info("Batch Summary "+batchRunData.toString());
     }
 }
